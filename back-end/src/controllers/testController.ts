adc358dd69bb6f2fd3e5b7d3297f5113eb0505ce
iagod <iago.tostes@cubos.io>
2022-07-01 12:12:15 -0300

    Preparando revisão de código

--- back-end/src/controllers/testController.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { prisma } from "../database.js";
-
-export async function clearRecommendations(_req: Request, res: Response) {
-  await prisma.recommendation.deleteMany(); // TODO Ask mentor if it is needed to follow layered architecture for this line
-
-  return res.sendStatus(200);
-}
